7c97af7d28b558105d3952d357b86d94fc7dc628
Ian Fisk <i.fisk32@gmail.com>
2018-03-26 02:14:20 -0700

    Deploy to github pages.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { PureComponent } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
 import { SyncLoader } from 'react-spinners';
 import IoNaviconRound from 'react-icons/lib/io/navicon-round';
 import DropdownButton from './components/dropdown-button';
